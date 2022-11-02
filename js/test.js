4c49d3ca1224b5066d94634c2936760d04d0e9bc
Michail <Gismanus@gmail.com>
2022-11-02 18:04:33 +0300

    filter functions

--- js/test.js
+++ js/test.js
@@ -1,25 +1,8 @@
-function pow(x, n) {
-    if (n == 1) {
-        return x;
-    } else {
-        return x * pow(x, n - 1);
-    }
+// let sum(a)(b) = a + b
+function sum(a){
+    return (function(b){
+        return a + b;
+    });
 };
 
-console.log(pow(2, 3));
-
-// Context: {x: 2, n: 3, string 5} (Call: pow(2, 3)); => new context
-
-// Context: {x: 2, n: 2, string 1} (Call: pow(2, 2)); => new context
-// Context: {x: 2, n: 3, string 5} (Call: pow(2, 3)); => old context saved in stack
-
-// Context: {x: 2, n: 1, string 1} (Call: pow(2, 1)); => this new context returns 2 and finish call with deleting this context;
-// Context: {x: 2, n: 2, string 1} (Call: pow(2, 2)); => old context saved in stack
-// Context: {x: 2, n: 3, string 5} (Call: pow(2, 3)); => old context saved in stack
-
-// Context: {x: 2, n: 2, string 1} (Call: pow(2, 2)); => this old context returns 4
-// and finish call with deleting this context;
-// Context: {x: 2, n: 3, string 5} (Call: pow(2, 3)); => old context saved in stack
-
-// Context: {x: 2, n: 3, string 1} (Call: pow(2, 3)); => this old context returns 8
-// and finish call with deleting this context;
+console.log(sum(1)(6));
